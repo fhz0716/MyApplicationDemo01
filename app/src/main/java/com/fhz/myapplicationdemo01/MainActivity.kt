5ef5964c66c0ef1bf222798b83d14785946a62e6
fuhz <1243593313@qq.com>
2021-05-15 11:57:05 +0800

    实现将一个activity的内容带到另一个activity中，通过点击事件触发

--- app/src/main/java/com/fhz/myapplicationdemo01/MainActivity.kt
+++ app/src/main/java/com/fhz/myapplicationdemo01/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fhz.myapplicationdemo01
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun sendMessage(view: View) {
-        val tv = findViewById<TextView>(R.id.tv)
+        val tv = findViewById<EditText>(R.id.editText1)
         val toString = tv.text.toString()
         val apply = Intent(this, Text01Activity::class.java).apply {
             putExtra(EXTRA_MESSAGE, toString)
